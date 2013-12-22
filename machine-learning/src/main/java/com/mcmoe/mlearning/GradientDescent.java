733dcc3ab5c9e45a39a727c7736d99824b78b892
mcmoe <mcxmoe@gmail.com>
2013-12-22 20:32:22 +0100

    Adding Gradient Descent pseudocode

---
+++ machine-learning/src/main/java/com/mcmoe/mlearning/GradientDescent.java
@@ -0,0 +1,12 @@
+package com.mcmoe.mlearning;
+
+public class GradientDescent {
+   public static void main (String[] args) {
+      // give me a list of x(i)s and ys
+      // pick alpha
+      // pick staring thetas
+      // define multivariate linear regression function
+      //    its computation should consider using matrix multiplication
+      // minimize using derivative of cost function
+   }
+}
