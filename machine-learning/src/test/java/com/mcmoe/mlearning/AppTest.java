71ecd3505bf84bf419d780c272269385a8978fbe
mcmoe <mcxmoe@gmail.com>
2013-12-22 20:21:33 +0100

    Setting up simple maven project

---
+++ machine-learning/src/test/java/com/mcmoe/mlearning/AppTest.java
@@ -0,0 +1,20 @@
+package com.mcmoe.mlearning;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest { 
+
+   /**
+    * Rigorous Test :)
+    */
+   @Test
+   public void testApp()
+   {
+      assertTrue("Should be true", true);
+   }
+}
